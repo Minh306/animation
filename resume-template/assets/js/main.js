5ce2f919fba152ed7cd75cceb8c47951a6a907bf
unknown <minh.le260899@gmail.com>
2021-04-13 16:04:19 +0700

    new code

--- resume-template/assets/js/main.js
+++ resume-template/assets/js/main.js
@@ -1,9 +1,9 @@
 /* SHOW MENU */
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav) {
+    if (toggle && nav) {
         toggle.addEventListener('click', () => {
             // add show-menu div tag nav__menu
             nav.classList.toggle('show-menu')
@@ -32,12 +32,12 @@ function scrollActive() {
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop -50;
+        const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else {
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
@@ -46,13 +46,13 @@ function scrollActive() {
 window.addEventListener('scroll', scrollActive)
 
 /* SHOW SCROLL TOP */
-function scrollTop(){
+function scrollTop() {
     const scrollTop = document.getElementById('scroll-top');
 
     // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 200) 
-        scrollTop.classList.add('show-scroll'); 
-    else 
+    if (this.scrollY >= 200)
+        scrollTop.classList.add('show-scroll');
+    else
         scrollTop.classList.remove('show-scroll')
 }
 
@@ -62,6 +62,7 @@ window.addEventListener('scroll', scrollTop)
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
+const darkPreloader = 'dark-preloader'
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
@@ -73,14 +74,18 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moo
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+    const preloader = document.getElementById("preloader")
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    preloader.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkPreloader)
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
+    const preloader = document.getElementById("preloader")
     // Add or remove the dark / icon theme
+    preloader.classList.toggle(darkPreloader)
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
     // We save the theme and the current icon that the user chose
@@ -90,12 +95,20 @@ themeButton.addEventListener('click', () => {
 
 /* REDUCE THE SIZE AND PRINT ON AN A4*/
 function scaleCv() {
+    let apresenteichan = document.getElementById("apresenteichan")
+    let homeTitle = document.getElementById("home__title")
     document.body.classList.add('scale-cv')
+    apresenteichan.classList.add('scale-title')
+    homeTitle.classList.add('scale-name')
 }
 
 /* REMOVE SIZE WHEN CV IS DOWNLOAD*/
 function removeScale() {
+    let apresenteichan = document.getElementById("apresenteichan")
+    let homeTitle = document.getElementById("home__title")
     document.body.classList.remove('scale-cv')
+    apresenteichan.classList.remove('scale-title')
+    homeTitle.classList.remove('scale-name')
 }
 
 
@@ -108,14 +121,14 @@ let resumeButton = document.getElementById('resume-button')
 
 //HTML2PDF OPTIONS
 let opt = {
-    margin:       0,
-    filename:     'myfile.pdf',
+    margin: 0,
+    filename: 'LeQuangMinh-CV.pdf',
     image: { type: 'jpeg', quality: 1 },
     html2canvas: {
-      dpi: 192,
-      scale:4,
-      letterRendering: true,
-      useCORS: true
+        dpi: 192,
+        scale: 4,
+        letterRendering: true,
+        useCORS: true
     },
     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
 };
@@ -136,3 +149,136 @@ resumeButton.addEventListener('click', () => {
     // 3. the .scale-cv class is remove from the body after 5 second to normal
     setTimeout(removeScale, 5000)
 })
+
+// HOVER IMAGE
+var getImage1 = document.getElementsByClassName("project-img") [0];
+var getImage2 = document.getElementsByClassName("project-img") [1];
+var getImage3 = document.getElementsByClassName("project-img") [2];
+// var getImage4 = document.getElementsByClassName("project-img") [3];
+
+function hoverIn(img) {
+    img.style.opacity = "1";
+    img.style.visibility = "visible";
+    // img.style.transition = "opacity 1s ease-in";
+}
+
+function hoverOut(img) {
+    img.style.opacity = "0";
+    img.style.visibility = "hidden";
+    // img.style.transition = "opacity 1s ease-in";
+}
+
+TweenMax.to(".preloader", 2.5, {
+    opacity: 0,
+    x: 60,
+    ease: Expo.easeInOut
+})
+TweenMax.to(".preloader-outer", 2, {
+    delay: 1,
+    left: "150%",
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".skills__content span", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".skills__content ul li", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.from(".change-theme", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+})
+
+TweenMax.from(".generate-pdf", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+})
+
+TweenMax.from(".home__data img", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+})
+
+TweenMax.from(".home__title b", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+})
+
+TweenMax.from(".home__profession", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".home__information", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".social__link", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.from(".profile__description", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".education__time .education__rounder", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.from(".education__line", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".education__data h3", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".education__data span", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".experience__data h3", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+TweenMax.staggerFrom(".experience__data span", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+TweenMax.staggerFrom(".experience__data p", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+TweenMax.staggerFrom(".experience__data a", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".experience__rounder", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".experience__line", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".certificate__content h3", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".certificate__content p", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".languages__content li", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".languages__content span", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".interests__content i", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".interests__content span", 1, {
+    delay: 2, opacity: 0, x: -30, ease: Expo.easeInOut
+}, 0.2)
+
+
+
